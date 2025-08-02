73b4ef3faeff773a431456bf9f6948919a898649
*** Lucas <jmlucasdelamo@gmail.com>
2025-08-02 21:12:04 +0200

    Operadores aritméticos

---
+++ operadoresAritmeticos.js
@@ -0,0 +1,77 @@
+// SUMA +
+
+// EL OPERADOR DE LA SUMA '+', SÍ SE APLICA A DOS OPERANDOS, LAS CONVIERTE EN RESULTADO QUE LA SUMA ENTRE ELLOS.
+
+console.log(5 + 2); // 7, el resultado de la suma
+console.log(8 + 3); // 11, el resultado de la suma
+console.log(8 + 4); // 12, el resultado de la suma
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// RESTA -
+
+// EL OPERADOR DE LA RESTA '-', SÍ SE APLICA A DOS OPERANDOS, LAS CONVIERTE EN RESULTADO QUE LA RESTA ENTRE ELLOS.
+
+console.log(5 - 2); // 3, el resultado de la resta
+console.log(8 - 3); // 5, el resultado de la resta
+console.log(8 - 4); // 4, el resultado de la resta
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// MULTIPLICACIÓN *
+
+// EL OPERADOR DE LA MULTIPLICACIÓN '*', SÍ SE APLICA A DOS OPERANDOS, LAS CONVIERTE EN RESULTADO DE UNA MULTIPLICACIÓN, POR LO CUAL OBTENEMOS EL PRODUCTO.
+
+console.log(5 * 2); // 10, el resultado de la multiplicación
+console.log(8 * 3); // 24, el resultado de la multiplicación
+console.log(8 * 4); // 32, el resultado de la multiplicación
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// DIVISIÓN /
+
+// EL OPERADOR DE LA DIVISIÓN '*', SÍ SE APLICA A DOS OPERANDOS, LAS CONVIERTE EN RESULTADO DE UNA DIVISIÓN, POR LO CUAL OBTENEMOS EL RESULTADO.
+
+console.log(5 / 5); // 1, el resultado de la división
+console.log(8 / 2); // 4, el resultado de la división
+console.log(8 / 4); // 2, el resultado de la división
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// RESTO %
+
+// EL OPERADOR RESTO %, A PESAR DE SU APARIENCIA, NO ESTÁ RELACIONADO CON PORCENTAJES.
+
+// EL RESULTADO DE 'a % b' ES EL RESTO DE LA DIVISIÓN ENTERA DE 'a' POR 'b'.
+
+// POR EJEMPLO:
+
+console.log(5 % 2); // 1, es el resto de 5 dividido por 2
+console.log(8 % 3); // 2, es el resto de 8 dividido por 3
+console.log(8 % 4); // 0, es el resto de 8 dividido por 4
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// EXPONENCIACIÓN **
+
+// EL OPERADOR EXPONENCIACIÓN 'a ** b' ELEVA A LA POTENCIA DE B.
+
+// POR EJEMPLO:
+
+console.log(2 ** 2); // 2² = 4
+console.log(2 ** 3); // 2³ = 8
+console.log(2 ** 4); // 2⁴ = 16
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// RAIZ CUADRADA ½
+
+// MATEMÁTICAMENTE, LA EXPONENCIACIÓN ESTÁ DEFINIDA PARA OPERADORES NO ENTEROS TAMBIÉN.
+
+// POR EJEMPLO, LA RAÍZ CUADRADA ES EL EXPONENTE ½.
+
+console.log(4 ** (1/2)); // 2 (potencia de 1/2 es lo mismo que raíz 
+console.log(8 ** (1/3)); // 2 (potencia de 1/3 es lo mismo que raíz cúbica)
+console.log(16 ** (1/4)); // 2 (potencia de 1/4 es lo mismo que raíz
+
+// ------------------------------------------------------------------------------------------------------------------------
